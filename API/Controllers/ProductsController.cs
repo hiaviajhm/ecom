2c4e4d1a3efeda6e656c3e46cb62f1e6b7d13d89
hiaviajhm <57320907+hiaviajhm@users.noreply.github.com>
2021-01-14 17:34:09 -0500

    End of Section 7

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -44,7 +44,7 @@ namespace API.Controllers
             var data = _mapper
                 .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
 
-            return Ok(new Pagination<ProductToReturnDto>(productParams.PageSize,productParams.PageSize, totalItems, data));
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex,productParams.PageSize, totalItems, data));
         }
 
         [HttpGet("{id}")]
